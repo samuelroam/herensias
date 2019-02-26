260c219fda723aea96cbc38f4cd288b207631cda
ik013043z1 <ik013043z1@hz341926-206-16.H013043.NET>
2019-02-26 13:55:28 +0100

    asdf

---
+++ src/trabajadores/Main.java
@@ -0,0 +1,13 @@
+package trabajadores;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Persona> personas = new ArrayList<Persona>();
+		personas.setNombre("Alberto");
+
+	}
+
+}
